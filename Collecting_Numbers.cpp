8a10c80cd8026da76133c8aa3484fc2d203c4b59
destx0 <destisdbest@gmail.com>
2023-01-06 19:46:05 +0530

    ghjhjgjh

--- Collecting_Numbers.cpp
+++ Collecting_Numbers.cpp
@@ -14,6 +14,17 @@ using namespace std;
     while (_t--)
 int main() {
     IOS;
-
+    ll n, x, res = 1, check = 0;
+    cin >> n;
+    int a[n] = {0};
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        a[x] = i;
+    }
+    for (int i = 0; i < n; i++) {
+        if (a[i] < check) res++;
+        check = a[i];
+    }
+    cout << res << endl;
     return 0;
 }
