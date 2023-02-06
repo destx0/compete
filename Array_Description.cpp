8e2013b801d629000ec2a4163fd57ebf65bff4fa
destx0 <destisdbest@gmail.com>
2023-02-06 15:51:00 +0530

    dpp

---
+++ Array_Description.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int>
+#define endl "\n"
+#define MOD (1000000007)
+#define IOS                  \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define testcase \
+    int _t;      \
+    cin >> _t;   \
+    while (_t--)
+int main() {
+    IOS;
+    ll res = 0;
+
+    return 0;
+}
